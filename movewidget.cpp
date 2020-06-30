236b34b48fe0bcf125b8e52c3f45fd1749642f33
Paguz <pagottomanuel@gmail.com>
2020-07-01 00:25:01 +0200

    aggiunto setLayout

--- movewidget.cpp
+++ movewidget.cpp
@@ -30,6 +30,8 @@ MoveWidget::MoveWidget(QWidget *parent) : QWidget(parent)
     grid->addWidget(DOWN,1,1);
     grid->addWidget(LEFT,1,0);
     grid->addWidget(RIGHT,1,2);
+    
+    setLayout(grid);
 }
 
 void MoveWidget::dirPressed(char dir) {
