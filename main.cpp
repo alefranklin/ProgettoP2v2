c9872fe47580c00919c7186b929d2a62d2f6c54a
Matteo- <matteopagotto2@gmail.com>
2020-06-30 16:19:20 +0200

    agiiunto main

--- main.cpp
+++ main.cpp
@@ -3,13 +3,24 @@
 #include "game.h"
 
 #include <QApplication>
+#include <QSplashScreen>
+#include <QTimer>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    //mostro lo splash screen
+    QPixmap pixmap(":/splash/splash_img");
+    QSplashScreen splash(pixmap, Qt::WindowStaysOnTopHint);
+    splash.show();
+
     Game g;
     MainView w(&g);
     Controller c(&g, &w);
-    w.show();
+
+    QTimer::singleShot(200, &splash, SLOT(close()));   // chiudo lo spalsh screen
+    QTimer::singleShot(200, &w, SLOT(show()));         // e poi apro la finestra
+
     return a.exec();
 }
